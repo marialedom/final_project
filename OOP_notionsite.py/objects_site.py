6db9b0772711d58e5c0f086f1bfc38ef309d981e
Maria Dominguez <mdominguezgonzalez1@babson.edu>
2023-12-09 18:11:00 -0500

    OOP
    added some functions

--- OOP_notionsite.py/objects_site.py
+++ OOP_notionsite.py/objects_site.py
@@ -1,24 +1,55 @@
 # create a dictionary with all tasks = To-Do List
 
 to_do_list = {}
-filter = ["due date", "subject", "priority level", "status"]
+filters = ["due date", "subject", "priority level", "status"]
+status = {0: "To-Do", 1: "In Progress", 2: "Complete"}
+
+
+
 
 #simplified id
 
 class Task:
-    
-    def __init__(self, id, filter, status, priority_rating, title, description, subject, due_date, TOPICS = None):
+      
+    def __init__(self, task_id, filters, status, title, description, subject, due_date_s, TOPICS = None):
+        self.id = task_id ##no idea if this is correct, or best way to generate ids
+
         self.status = status
-        self.priorityrating = priority_rating
-        self.filter = filter
+
+        self.filter = filters
+
         self.title = title
+
         self.description = description
+
         self.subject = subject
-        self.duedate = due_date
-        self.id = id ##no idea if this is correct, or best way to generate ids
+
+        self.due_date_str = due_date_s #DUE DATE AS A STRING IN FORMAT
+
         if TOPICS == None:
             self.topics = []
+
+    def set_duedate(self, due_date_s: str):
+        import maya
+        self.due_date = maya.parse(due_date_s).datetime()
+        return self.due_date
+
+
     
+    def get_priority_rating(self, due_date_s):
+        """Function takes the current date and the due date of an assingment to rate it in priority from 1 (not priority) 5 (highest priority)
+        """
+        import maya 
+        # maya.parse(due_date_s).slang_time()
+        self.due_date = maya.parse(due_date_s).datetime()
+
+
+        self.priority_rating = self.duedate_rating 
+
+        return self.priority_rating
+
+
+
     def new_task_todo(self, id, filter, title, description, subject, due_date, TOPICS):
         
         to_do_list[self.id] = {"ID": self.id, "Title": self.title, "Description": self.description, "Subject": self.subject, "Due Date": self.due_date, "Topics": self.topics} #missing status and priority rating
@@ -27,7 +58,5 @@ class Task:
 
 
 
-
-        
 ### calculate priority level/order based on due date and priority rating
-    
+
