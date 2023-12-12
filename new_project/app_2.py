53afa9053f34579a3c8f05d6d48caf7c279cef60
Maria Dominguez <mdominguezgonzalez1@babson.edu>
2023-12-11 21:31:45 -0500

    SOME ERRORS
    ERROR 1: TASKS DONT SAVE
    ERROR 2: TASKS DON'T SHOW UP IN LIST MODE AND TEXT DOESNT WORK

--- new_project/app_2.py
+++ new_project/app_2.py
@@ -14,28 +14,14 @@ def index():
         subject = request.form['subject']
         due_date = request.form['due_date']
         topics = request.form['topics'].split(',') if request.form['topics'] else []
-        status = request.form['status']
+        status = request.form.get('status')  # Make sure to handle this in your Task class
 
         new_task = OOP_FUNCTIONS.Task(title, description, subject, due_date, topics)
         to_do_list.add_task(new_task)
-        return redirect(url_for('index'))
+        return redirect(url_for('index'))  # Redirect to avoid form resubmission issues
 
-    return render_template('dashboard.html', tasks=to_do_list.get_all_tasks())
-
-@app.route('/', methods=['GET', 'POST'])
-def task_adding():
-    if request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
-        subject = request.form['subject']
-        due_date = request.form['due_date']
-        topics = request.form['topics'].split(',') if request.form['topics'] else []
-
-        new_task = OOP_FUNCTIONS.Task(title, description, subject, due_date, topics)
-        to_do_list.add_task(new_task)
-        return redirect(url_for('index'))
-
-    return render_template('tasks_adding.html', tasks=to_do_list.get_all_tasks())
+    tasks = to_do_list.get_all_tasks()
+    return render_template('dashboard.html', tasks=tasks)
 
 @app.route('/filter_tasks', methods=['POST'])
 def filter_tasks():
@@ -44,6 +30,12 @@ def filter_tasks():
     filtered_tasks = to_do_list.filter_tasks(filter_type, filter_value)
     # Convert filtered_tasks to a suitable format for returning to the frontend
     return jsonify(filtered_tasks)
+
+@app.route('/get_calendar_tasks')
+def get_calendar_tasks():
+    tasks = to_do_list.get_all_tasks()
+    calendar_tasks = [{'title': task.title, 'start': task.due_date.strftime('%Y-%m-%d')} for task in tasks]
+    return jsonify(calendar_tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
